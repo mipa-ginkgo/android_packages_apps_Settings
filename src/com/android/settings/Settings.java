5e117453f9f6a4eb6afccfa6a135a5d5560b7239
maxwen <max.weninger@gmail.com>
2022-03-04 17:22:25 +0530

    settings: Build ArrowLab into Settings
    commit 122480e9795d751c48160e91ee51c54a884953e8
    Author: Alex Cruz <alex@dirtyunicorns.com>
    Date:   Sat Jul 17 18:53:20 2021 +0000
    
        Settings: Add expandable bottombar for extensions
    
        Github Repo:- https://github.com/st235/ExpandableBottomBar
    
        Co-authored-by: Shubham Singh <coolsks94@gmail.com>
        Signed-off-by: Shubham Singh <coolsks94@gmail.com>
        Change-Id: I310c396b474535ec1584ab164029755a4da18d84
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    commit b1a6e09bf2fd6d7933f6b38930b355df5062f8da
    Author: maxwen <max.weninger@gmail.com>
    Date:   Fri Sep 8 05:07:18 2017 +0530
    
        Build Extensions into Settings
    
          * Add it on dashboard
          * Set proper priority to show it below Apps & Notifications
    
        [jaysonedson: Ported to android11 and Android.bp]
    
        Signed-off-by: Shubham Singh <coolsks94@gmail.com>
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
        Change-Id: I594d89ad713a4735f8301c8e6372790812b2d86f
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -454,4 +454,7 @@ public class Settings extends SettingsActivity {
      * Activity for PreviouslyConnectedDeviceDashboardFragment
      */
     public static class PreviouslyConnectedDevicesActivity extends SettingsActivity { /* empty */ }
+
+    // Customizer Additions.
+    public static class ArrowlabSettingsActivity extends SettingsActivity {}
 }
